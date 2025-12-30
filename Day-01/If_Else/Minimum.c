c44addbe470a0d144263aa90da420c084c23a9c9
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2025-12-30 12:29:02 +0530

    30/12/2025

---
+++ Day-01/If_Else/Minimum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+    int num1,num2;
+
+printf("Enter Number 1 and Number 2:");
+scanf("%d%d",&num1,&num2);
+
+if(num1<num2){
+ printf("Number 1 is minimum");
+}else{
+ printf("Number 2 is minimum");
+}
+}
