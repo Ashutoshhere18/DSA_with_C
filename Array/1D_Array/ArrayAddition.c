c37bc9572101ee989967f8181cf68457211e0da4
Ashutoshhere18 <mishraashutoshwk@gmail.com>
2026-01-08 17:49:58 +0530

    08/01/2026

---
+++ Array/1D_Array/ArrayAddition.c
@@ -0,0 +1,48 @@
+// wap to perform the addition operation of two 
+
+
+#include<stdio.h>
+
+void main(){
+    int i,n,sum=0;
+    
+
+    printf("Array size:");
+    scanf("%d",&n);
+
+    int arr1[n];
+    int arr2[n];
+    int result[n];
+    
+    //input
+    for(i=0;i<n;i++){
+      printf("\nEnter arr1[%d]:",i);
+      scanf("%d",&arr1[i]);
+    }
+        for(i=0;i<n;i++){
+      printf("\nEnter arr2[%d]:",i);
+      scanf("%d",&arr2[i]);
+    }
+    //output
+    printf("\n array1:");
+   for(i=0;i<n;i++){
+     printf("%d,",arr1[i]);
+   }
+   printf("\n array2:");
+   for(i=0;i<n;i++){
+     printf("%d,",arr2[i]);
+   }
+  
+   //sum logic
+   for(i=0;i<n;i++){
+     sum=arr1[i]+arr2[i];
+     result[i]=sum;
+   }
+  
+   //printing resultant array
+   printf("\n");
+    printf("\n Result Array");
+   for(i=0;i<n;i++){
+     printf("%d,",result[i]);
+   }
+}
---
+++ Array/1D_Array/Print_n_Elements.c
@@ -0,0 +1,23 @@
+
+
+#include<stdio.h>
+
+void main(){
+    int i,n;
+
+    printf("Array size:");
+    scanf("%d",&n);
+
+    int arr[n];
+    
+    // For Input 
+    for(i=0;i<n;i++){
+      printf("\nEnter arr[%d]:",i);
+      scanf("%d",&arr[i]);
+    }
+  // For display
+   for(i=0;i<n;i++){
+     printf("%d,",arr[i]);
+   }
+  
+}
